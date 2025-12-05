f5a1b03e6fe470d7f26e9ef792e004e9205e1679
dubsbol <sambeequick@gmail.com>
2025-12-05 01:52:44 -0500

    color update/removed window radius/kitty theme. TODO:centralize colors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,7 +3,6 @@ polybar-msg cmd quit >/dev/null 2>&1
 killall -q polybar
 echo "----" | tee -a /tmp/dubsbar.log
 polybar -r dubsbar 2>&1 | tee -a /tmp/dubsbar.log & disown &
-picom --experimental-backends -b
+picom -b
 feh --no-fehbg --bg-fill -z $HOME/Pictures/WP/
 killall -q glava
-#glava --desktop
