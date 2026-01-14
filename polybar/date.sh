d995e5838934cea1634357c7889dcc1a23625cc3
dubsbol <sambeequick@gmail.com>
2026-01-14 04:34:03 -0500

    switching to dwm

--- polybar/date.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ncal
-read value
-exit
--- polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-polybar-msg cmd quit >/dev/null 2>&1
-killall -q polybar
-echo "----" | tee -a /tmp/dubsbar.log
-polybar -r dubsbar 2>&1 | tee -a /tmp/dubsbar.log & disown &
-picom -b
-feh --no-fehbg --bg-fill -z $HOME/Pictures/WP/
-killall -q glava
